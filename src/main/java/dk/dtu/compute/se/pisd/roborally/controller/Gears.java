094e220f91df29b7c517788b4014ec8af536c3ad
SkumJustEatMe <s214662@dtu.dk>
2023-05-01 12:07:39 +0200

    Added a Rotation Enum inside model packade, and a Gears constructor so the FieldsAction Gears can check what way we should rotate the player

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Gears.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Gears.java
@@ -1,18 +1,25 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.model.Rotation;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class Gears extends FieldAction{
 
+    private Rotation rot;
+
+    public Gears(Rotation rotation){
+        this.rot = rotation;
+    }
+
 
     @Override
     public boolean doAction(GameController gameController, Space space) {
-        if(space.rotation?){
+        if(this.rot == Rotation.CLOCKWISE){
             gameController.turnRight(space.getPlayer());
         }
 
 
-        if(space.rotation?){
+        if(this.rot == Rotation.COUNTERCLOCKWISE){
             gameController.turnLeft(space.getPlayer());
         }
 
