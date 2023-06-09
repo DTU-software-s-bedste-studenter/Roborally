a3936eee4099737b1d8ee6f9dd8891a1b1801b57
Anders Nørgaard <ansi1806@gmail.com>
2023-06-09 15:25:21 +0200

    small upsi

--- RoboRallySpringAPI/src/main/java/com/example/roborallyspringapi/RoboRallySpringApiApplication.java
+++ RoboRallySpringAPI/src/main/java/com/example/roborallyspringapi/RoboRallySpringApiApplication.java
@@ -1,6 +1,5 @@
 package com.example.roborallyspringapi;
 
-import com.example.roborallyspringapi.api.controller.FullBoardController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
